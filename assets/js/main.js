899784ddf413d0e7c304120158fa653f65db5a19
[muradnuriyev001] <[muradnuriyev001@gmail.com]>
2022-12-03 13:42:22 +0400

    js2

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+let stock1 = 15;
+let stock2 = 30;
+let stock3 = 25;
+function stockTake1(x){
+   while (stock1>0){
+      return stock1-=x
+   }
+}
+function stockTake2(x){
+   while (stock2>0){
+      return stock2-=x
+   }
+}
+function stockTake3(x){
+   while (stock3>0){
+      return stock3-=x
+   }
+}
+
